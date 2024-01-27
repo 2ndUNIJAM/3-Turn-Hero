348986576e06b199993d329ef83e435c72f593f2
Dantae An <48adt@naver.com>
2024-01-27 13:11:49 +0900

    add Weapon.cs, Armor.cs, Colleague.cs and add weapon.ElementFireEffect()

--- Assets/Scripts/Object/Unit/Unit.cs
+++ Assets/Scripts/Object/Unit/Unit.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,6 +17,13 @@ public class Unit : MonoBehaviour
     protected float knockBackPower;
     protected bool isHit;
     protected bool isDead;
+
+    // 지속 피해 데미지.
+    // 지속 피해량이 더 높아지는 효과가 새로 들어오는 경우 기존 효과를 갱신
+    // 지속 피해량이 같거나 낮은 효과가 새로 들어오는 경우 기존 효과의 지속 시간이 남아 있는 동안에는 아무 효과 없음
+    // 지속 시간은 어떤 효과든 3초.
+    // 지속 피해를 안 주고 있는 동안에는 dottedDamageAmount == 0
+    protected int dottedDamageAmount = 0;
 
     [SerializeField]
     protected Animator animator;
@@ -43,6 +50,7 @@ public class Unit : MonoBehaviour
 
     public virtual void AddMaxHP(int hp)
     {
+        if (isDead) return;
         changedStat.MaxHP += hp;
         changedStat.CurrentHP += hp;
     }
@@ -51,6 +59,7 @@ public class Unit : MonoBehaviour
 
     public virtual void ReduceHP(int damage)
     {
+        if (isDead) return;
         changedStat.CurrentHP -= damage;
         CheckDead();
         StartHitAnim();
@@ -58,6 +67,8 @@ public class Unit : MonoBehaviour
 
     public virtual void ReduceHPPercent(float percent)
     {
+        if (isDead) return;
+
         // 최대 체력 비례 데미지
         changedStat.CurrentHP -= Mathf.RoundToInt(changedStat.MaxHP * percent);
         CheckDead();
@@ -92,5 +103,20 @@ public class Unit : MonoBehaviour
         rigidbody.velocity = Vector2.zero;
         animator.SetBool("isHit", false);
         isHit = false;
+    }
+
+    public IEnumerator DottedDamage(int damage)
+    {
+        if (dottedDamageAmount >= damage) yield break;
+
+        dottedDamageAmount = damage;
+
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            ReduceHP(damage);
+            if (isDead) yield break;
+        }
+        dottedDamageAmount = 0;
     }
 }
